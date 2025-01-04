27662d395a6b07f2afbb3d5379d1d1a4be97eeec
Anuska <161431021+Nushhhhhhh@users.noreply.github.com>
2025-01-04 12:07:51 +0545

    Delete portfolio/week1 directory

--- portfolio/week1/1_2.py
+++
@@ -1,6 +0,0 @@
-# Make a copy of the previous program, and modify it so that it displays your name. So if your name is Herbert the message should become:
-# Hello, Herbert!
-
-name = "Anuska"
-print(f"Hello, {name}!")
-
